ac98f5bb1437520180b9b7044a1768814ec8f3cb
abiudev <bdmasinde@gmail.com>
2024-10-08 07:28:44 -0700

    updated image links

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,10 +1,10 @@
 export const images = [
-  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg",
   "https://images.pexels.com/photos/1741205/pexels-photo-1741205.jpeg",
   "https://images.pexels.com/photos/20787/pexels-photo.jpg",
-  "https://www.pexels.com/photo/low-angle-shot-of-a-tabby-cat-208984/",
-  "https://www.pexels.com/photo/european-shorthair-cat-on-a-woven-basket-1543793/",
-  "https://www.pexels.com/photo/cute-tabby-kitten-on-a-sofa-2194261/",
-  "https://www.pexels.com/photo/grey-fur-kitten-127028/",
-  "https://www.pexels.com/photo/person-holding-white-kitten-with-flowers-necklace-1643457/",
+  "https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg",
+  "https://images.pexels.com/photos/57416/cat-sweet-kitty-animals-57416.jpeg?",
+
+  "https://images.pexels.com/photos/774731/pexels-photo-774731.jpeg?",
+  "https://images.pexels.com/photos/1543793/pexels-photo-1543793.jpeg",
+  "https://images.pexels.com/photos/2194261/pexels-photo-2194261.jpeg",
 ];
